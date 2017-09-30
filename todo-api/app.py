1a219d7e9b00e55508387d1fb3080b62e03bf7bc
Macbook <nalinsuri@gmail.com>
2017-09-30 17:41:18 -0400

    Added unknown and known. Need to modularize better....

--- todo-api/app.py
+++ todo-api/app.py
@@ -1,45 +1,58 @@
-#!flask/bin/python
-from flask import Flask, request, redirect, url_for
+#!venv/bin/python
+from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
 
-UPLOAD_DIRECTORY = '/ShellHacks/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
-
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit(
-		'.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-def uploadPicture(picture, is_known=True):
-    if is_known is True:
-        UPLOAD_DIRECTORY += 'known'
-    else:
-        UPLOAD_DIRECTORY += 'unknown'
-
-    app.config['UPLOAD_DIRECTORY'] = UPLOAD_DIRECTORY
-    
-    filename = secure_filename(picture.filename)
-    picture.save(os.path.join(app.config['UPLOAD_DIRECTORY'], filename))
-    return redirect(url_for('uploaded_file', filename = filename))
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/unknown_upload', methods=['POST'])
-def unknown_upload():
-    if 'file' not in request.files:
-        flash('No file uploaded')
-        return redirect(request.url)
-    file = request.files['file']
-    
-    if file.filename == '':
-        flash('No file uploaded')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        return uploadPicture(file, False)
+def unknown_pic():
+    UPLOAD_FOLDER = '/ShellHacks2017/unknown'
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return
+
+@app.route('/known_upload', methods=['POST'])
+def known_pic():
+    UPLOAD_FOLDER = '/ShellHacks2017/known'
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return
+
 
 if __name__ == '__main__':
     app.run(debug=True)
